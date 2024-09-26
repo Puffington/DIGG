8630563fb8647f8774fe56f9f4e05a07e278d61b
dane743 <dane2200@student.miun.se>
2024-09-26 14:38:46 +0200

    uppdate med miun

--- connect.php
+++ connect.php
@@ -1,9 +1,14 @@
 <?php
 
-    $dbServerName = "localhost";
-    $dbUserName = "root";
-    $dbPassword = "";
-    $dbName = "DIGG";
+    //$dbServerName = "localhost";
+    //$dbUserName = "root";
+    //$dbPassword = "";
+    //$dbName = "DIGG";
+
+    $dbServerName = "studentmysql.miun.se";
+    $dbUserName = "dane2200";
+    $dbPassword = "j1tlo375";
+    $dbName = "dane2200";
 
     $conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
 
