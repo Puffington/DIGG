af65472186d8711b9fe7f129befab49d6c38f631
niwo2001 <nikki.wolgers@gmail.com>
2024-10-04 21:27:35 +0200

    added icon of house
    dosent work

--- index.php
+++ index.php
@@ -6,11 +6,14 @@
 <html lang="en">
 
 <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" type="text/css" href="css/style.css"> <!-- external css, a general css theme from the "style.css" -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Digg Förtroendemodellen</title>
+
+    
 </head>
 
 <body>
@@ -25,6 +28,7 @@
     <main>
         <div class="theMainDiv">
             <nav>
+                <i class="fa-light fa-house"></i>
                 <a href="#">Förtroendemodellen</a>
             </nav>
             <div class="headline">
