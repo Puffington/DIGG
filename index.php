5cd2e17ceb7a862e0a4f300a9c84bbf0d17ef41d
niwo2001 <nikki.wolgers@gmail.com>
2024-10-04 17:59:44 +0200

    added templates, header.php and footer.php

--- index.php
+++ index.php
@@ -1,22 +1,12 @@
 <?php
-    include("connect.php");
+    #include("includes/connect.php");
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <link rel="stylesheet" type="text/css" href="css/style.css"> <!-- external css, a general css theme from the "style.css" -->
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Typical Website Layout</title>
-
-<!-- personalized style css for this site only -->
-    <style> 
-
-    </style>
-
-</head>
+<?php
+    include("templates/header.php");
+?>
 <body>
 
     <!-- Header Section -->
@@ -49,9 +39,9 @@
     </main>
 
     <!-- Footer Section -->
-    <footer>
-        <p>&copy; 2024 Our Website | All rights reserved.</p>
-    </footer>
+    <?php
+        include("templates/footer.php");
+    ?>
 
 </body>
 </html>
