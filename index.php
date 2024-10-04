c9ae55534ba369364a7fd58a8460fc89097708a6
niwo2001 <nikki.wolgers@gmail.com>
2024-10-04 22:57:52 +0200

    text explaining the trust model

--- index.php
+++ index.php
@@ -38,6 +38,14 @@
                 </div>
             </div>
 
+            <div class="headline">
+                <h1>Vad är förtroendemodellen?</h1>
+            </div>
+
+            <div  class="textTrustModel">
+                <p>The trust model for artificial intelligence (AI) is a tool for self-evaluation of the use of AI by actors in the public sector. The model has several purposes. The main aim is to preserve, and even increase, the openness and transparency that exists within Swedish public administration. As a general rule, a completed trust model must be able to be shown to supervisory authorities and the interested public. In some cases, an actor is required to show that AI has been used. By using the trust model, you who want to use AI in your business also get increased security that rules and approaches that need to be taken into account when using AI are taken into account. Thus, the trust model can also serve the internal business development.</p>
+            </div>
+
         </div>
     </main>
 
