6ead57b5c862c13a234981b858a4efea35fef0b8
Theo <36028733+Puffington@users.noreply.github.com>
2024-10-19 18:19:06 +0200

    removed pdf from db functions

--- includes/db_functions.php
+++ includes/db_functions.php
@@ -37,39 +37,6 @@ if (isset($_POST["AI"])) {
         $database->insertIntoOrganisationTable($ID, $ORGN_NR, $NAME);
     }
     exit();
-}elseif(isset($_POST["PDF"])){
-    echo("you got to the right area!!! hosam can code here");
-    
-
-    //QUESTIONS - has all the JSON questions, Parse them and then create pdf from them
-    //ANSWERS - has all the answers from the form, in the form  of {ID,VALUE}, use  a  "for each" os similar to iterate through each answer
-    //ex. having OBJECT.ID will give value you can even iterate through every ID from the questions if you want
-
-    $ANSWERS = json_decode($_POST["ANSWERS"], true);
-    $QUESTIONS = json_decode(file_get_contents("questions.json"),true);
-
-    #echo("answers:  ". json_encode($ANSWERS)); //here you can see the structure of answers
-    #echo("   questions:  ".json_encode($QUESTIONS)); //here you can see the structure of questions
-
-    foreach ($ANSWERS as $key => $value) {
-        echo( $key ." - ". $value . "   ");
-    }
-
-    if ($QUESTIONS !== null && is_array($QUESTIONS)) {
-        // Loop through the array of objects
-        foreach ($QUESTIONS as $object) {
-            if(isset($object['id']))
-            echo("id ".$object['id']);
-        }
-    } else {
-        // Handle error (e.g., log, show error message)
-        echo "Failed to decode JSON or no data available.";
-    }
-
-
-
-
-
 }
 
 
